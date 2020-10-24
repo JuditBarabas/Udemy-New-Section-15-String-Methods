bbba79d40ed69debedae1427bb2c3f86edc2ecae
JuditBarabas <62601033+JuditBarabas@users.noreply.github.com>
2020-10-24 23:05:26 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log('For solution open js file');
+
+// prewritten
+const message = "    TASTE THE RAINBOW!  ";
+
+// YOUR CODE BELOW THIS LINE:
+
+let whisper = message.trim().toLowerCase();
